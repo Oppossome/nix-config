140c59e5aec5e81145d135e8be5882d132ffeb57
Cutler <dsecutler@gmail.com>
2026-06-10 18:49:44 -0400

    Add Steam UDev rules

--- modules/programs/gaming/steam.nix
+++ modules/programs/gaming/steam.nix
@@ -8,5 +8,13 @@
 		programs.steam.remotePlay.openFirewall = true;
 		programs.steam.dedicatedServer.openFirewall = true;		
 		programs.gamemode.enable = true;
+
+		# Enable uinput kernel module (required for many controllers)
+		hardware.uinput.enable = true;
+
+		# Add udev rules for Steam input devices
+		services.udev.packages = with pkgs; [
+			steam-devices-udev-rules
+		];
 	};
 }
